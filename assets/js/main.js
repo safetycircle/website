99eecdf7b5bfec8256beb41bd93973b168b287c5
ramiyahya <contact@ramiyahya.com>
2014-06-05 13:07:11 +1000

    Fixed break on small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,36 @@ jQuery(document).ready(function($) {
         $(this).tab('show')
     })
 
+  //   var videoModal = $('#video-modal'),
+		// videoSource = $('#video-container iframe');
+
+  //   $('a.video-modal-trigger').on('click', function (e) {
+
+  //       // Load in video source dynamically 
+  //       var src = $(this).attr('data-src');
+
+  //       videoSource.attr({
+  //           'src': src
+  //       });
+
+  //       // Trigger modal window
+  //       videoModal.modal();
+
+  //       e.preventDefault();
+  //   });
+
+  //   $('a.close-video-modal').on('click', function (e) {
+  //       var videoContainer = $('#video-container');
+
+  //       // Hide modal window
+  //       videoModal.modal('hide');
+
+  //       // Remove video from Modal
+  //       videoSource.attr({
+		// 	'src': ''
+  //       });
+
+  //       e.preventDefault();
+  //   });
+
 });
